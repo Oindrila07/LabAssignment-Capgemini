6b250cbad95d0eee867a11b7fc61999e0501f224
Oindrila07 <84120160+Oindrila07@users.noreply.github.com>
2021-06-06 21:58:38 +0530

    Add files via upload

---
+++ Lab 7/EmployeeServiceImplementation.java
@@ -0,0 +1,22 @@
+package com.cg.eis.service;
+
+import com.cg.eis.bean.Employee;
+
+
+import com.cg.eis.pl.EmployeeHash;
+public class EmployeeServiceImplementation implements EmployeeService {
+    EmployeeHash ep = new EmployeeHash();
+
+    public boolean addEmployee(Employee e) {
+        return ep.addEmployee(e);
+    }
+
+    public String findInsuranceScheme(double sal, String desg) {
+        return ep.findInsuranceScheme(sal, desg);
+
+    }
+
+    public Employee showEmployee(int id) {
+        return ep.showEmployee(id);
+    }
+}
